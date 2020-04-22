ca7c1fc641de3de810bc23f9fbe8b792d836847f
Sam Broyles <s.broyles@students.clark.edu>
2020-04-22 14:55:56 -0700

    Completed code. Header updated

---
+++ Hello.py
@@ -0,0 +1,19 @@
+"""
+CTEC 121
+Sam Broyles
+First Program Lab   
+First Hello Program
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    print()
+    print("Hello World!")
+    print()
+
+main()    
